a40f189ad59b087b1a0e5ec99b04ecab61f33323
harrywells1 <71169884+harrywells1@users.noreply.github.com>
2021-08-12 12:35:07 +1200

    Draw Dots

---
+++ drawdots.py
@@ -0,0 +1,25 @@
+import turtle
+
+WIDTH, HEIGHT = 1400, 560
+
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)  # fudge factors due to window borders & title bar
+screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
+
+
+flag = turtle.Turtle()
+
+start=160
+flag.penup()
+flag.goto(0,50)
+for i in range(2):
+    for j in range (5):
+        flag.penup()
+        flag.forward(160)
+        flag.dot()
+        flag.penup()
+
+    flag.goto(0, start)
+    start = start + 50
+turtle.done()
+
