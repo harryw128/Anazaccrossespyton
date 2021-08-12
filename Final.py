16b404b7d852c0a2f3ce44d941bf92adaaaa060e
harrywells1 <71169884+harrywells1@users.noreply.github.com>
2021-08-12 19:56:32 +1200

    Merged new draw dots file in

--- Final.py
+++ Final.py
@@ -13,27 +13,29 @@ print(crossnames)
 
 
 
+#Width and height for turtles
 WIDTH, HEIGHT = 1400, 560
 
+#Setting up where turtle starts
 screen = turtle.Screen()
-screen.setup(WIDTH, HEIGHT)  # fudge factors due to window borders & title bar
-screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
+screen.setup(WIDTH, HEIGHT)  #sets up window
+screen.setworldcoordinates(0, 0, WIDTH, HEIGHT) #turtle boundires
 
 
-flag = turtle.Turtle()
+flag = turtle.Turtle() #Naming turtle
 
-start=160
-flag.penup()
-flag.goto(0,50)
-for i in range(2):
-    for j in range (5):
+start=200
+flag.penup() #So turtle doesnt draw lines
+flag.goto(0,100) #Tells turtle where to start
+for i in range(2): #Loops twice so does 2 lines of dots
+    for j in range (5): #Loops 5 times to do 5 dots
         flag.penup()
-        flag.forward(160)
-        flag.dot()
-        flag.penup()
+        flag.forward(160) #Move forward for dots
+        flag.dot() #Places Dot
+        flag.penup() #Puts dot back up
 
-    flag.goto(0, start)
-    start = start + 50
+    flag.goto(0, start) #Tells turtle where to go for 2nd line
+    start = start + 100 #Adds 100 so turtle goes higher
 
 liz = turtle.Turtle()
 start=160
