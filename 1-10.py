f2eca9c16bf74b3601f49192e90728d5bf76621f
harrywells1 <71169884+harrywells1@users.noreply.github.com>
2021-08-12 12:42:52 +1200

    n

--- 1-10.py
+++
@@ -1,7 +0,0 @@
-for x in range(11):
-    if x == 11: break
-    print (x)
-else:
- print("Finally finished!")
-
-
--- Lists.py
+++
@@ -1,9 +0,0 @@
-# This program adds two numbers
-y = [2,3,5,7]
-x = [4,7,1,8]
-
-# Add two numbers
-sum = y + x
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(y, x, sum))
