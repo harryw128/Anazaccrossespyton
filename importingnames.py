b9edd6a2d7d5a5eac3c302972a4c387023a543e3
harrywells1 <71169884+harrywells1@users.noreply.github.com>
2021-08-12 12:46:49 +1200

    Added comments to importing name code

--- importingnames.py
+++ importingnames.py
@@ -1,11 +1,11 @@
+#opens crosses fill and read lines
 with open('crosses.txt') as f:
     lines = f.readlines()
 print(lines)
+#prints out names from txt file
 
+#removes /n after all names
 new_list = [s.replace("\n", "") for s in lines]
 
 print(new_list)
-
-sorted_list = sorted(new_list)
-
-print(sorted_list)
+#prints out new list
